f973f58e62a4c827bcfef9f096e0dcf42812aab6
岸直哉 <user.id.125125@gmail.com>
2021-01-24 20:06:45 +0900

    bibary_search

--- search/binary_search.py
+++ search/binary_search.py
@@ -7,7 +7,7 @@ li.sort()
 def binary_search(nums, value):
     left, right = 0, len(nums) - 1
 
-    while right >= left:
+    while right > left:
         mid = (right + left) // 2
         
         if nums[mid] == value:
@@ -20,3 +20,20 @@ def binary_search(nums, value):
     return -1
 
 binary_search(li, 5)
+
+def binary_search_recursion(nums, value):
+    def inner(nums, value, left, right):
+        mid = (right + left) // 2
+        
+        if left > right:
+            return -1
+
+        if nums[mid] == value:
+            return mid
+        elif nums[mid] < value:
+            return inner(nums, value, mid + 1, right)
+        else:
+            return inner(nums, value, left, mid - 1)
+    return inner(nums, value, 0, len(nums) - 1)
+
+binary_search_recursion(li, 5)
