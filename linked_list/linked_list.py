652830b4787847fea0b8c728db3fa57469a2bdd3
岸直哉 <user.id.125125@gmail.com>
2021-01-24 23:29:35 +0900

    linked_sort

---
+++ linked_list/linked_list.py
@@ -0,0 +1,20 @@
+class Node:
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next = next_node
+
+
+class LinkedList:
+    def __init__(self, head=None):
+        self.head = head
+    
+    def appned(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
