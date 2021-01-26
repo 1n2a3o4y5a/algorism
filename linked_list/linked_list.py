92e37a75e070a7fd322c92fdbbf21f72941d57f0
岸直哉 <user.id.125125@gmail.com>
2021-01-27 00:26:39 +0900

    remove

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -23,3 +23,31 @@ class LinkedList:
         new_node = Node(data)
         new_node.next = self.head
         self.head = new_node
+
+    def print(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.data)
+            current_node = current_node.next
+    
+    def remove(self, data):
+        current_node = self.head
+
+        if current_node and current_node.data == data:
+            self.head = current_node.next
+        previous_node = None
+        while current_node and current_node.data != data:
+            previous_node = current_node
+            previous_node.next = current_node.next
+        if current_node is None:
+            return
+        previous_node.next = current_node.next
+
+
+
+li = LinkedList()
+li.appned(1)
+li.appned(2)
+li.insert(0)
+
+li.print()
