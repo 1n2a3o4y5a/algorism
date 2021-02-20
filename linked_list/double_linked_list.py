bc6db3f186974a2921a3f1acd2fb3e5687dfc427
岸直哉 <user.id.125125@gmail.com>
2021-02-20 19:33:15 +0900

    doublelinkedlist

--- linked_list/double_linked_list.py
+++ linked_list/double_linked_list.py
@@ -18,7 +18,66 @@ class DoubleLinkedList:
         while current_node.next is not None:
             current_node = current_node.next
         current_node.next = new_node
-        current_node.previous = current_node
+        new_node.previous = current_node
+    
+    def insert(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+
+        self.head.previous = new_node
+        new_node.next = self.head
+        self.head = new_node
+    
+    def print(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.data)
+            current_node = current_node.next
+    
+    def remove(self, data):
+        current_node = self.head
+        if current_node and current_node.data == data:
+            if current_node.next is None:
+                current_node = None
+                self.head = None
+                return
+            else:
+                next_node = current_node.next
+                next_node.previous = None
+                current_node = None
+                self.head = next_node
+                return
+        
+        while current_node and current_node.data != data:
+            current_node = current_node.next
+        
+        if current_node is None:
+            return
+        
+        if current_node.next is None:
+            previous = current_node.previous
+            previous.next = None
+            current_node = None
+            return
+        else:
+            next_node = current_node.next
+            previous_node = current_node.previous
+            previous_node.next = next_node
+            next_node.previous = previous_node
+            current_node = None
+            return
+        
 
 li = DoubleLinkedList()
 li.appned(1)
+li.appned(2)
+li.appned(3)
+li.insert(0)
+li.appned(4)
+li.appned(5)
+li.print()
+print(11111111111111)
+li.remove(2)
+li.print()
