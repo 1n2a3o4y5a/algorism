7d52bb853ef0c3926536b78acc3982dc2702dda5
岸直哉 <user.id.125125@gmail.com>
2021-03-23 20:48:08 +0900

    ハッシュテーブル

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -11,8 +11,24 @@ class HashTable(object):
 
     def add(self, key, value):
         index = self.hash(key)
-        self.table[index].append([key, value])
+        for d in self.table[index]:
+            if d[0] == key:
+                d[1] = value
+                break
+        else:
+            self.table[index].append([key, value])
+
+    def get(self, key):
+        index = self.hash(key)
+        for d in self.table[index]:
+            if d[0] == key:
+                return d[1]
+        return None
 
 
 hash_table = HashTable()
 hash_table.add('pc', 'mac')
+hash_table.add('sns', 'youtube')
+hash_table.add('sns', 'mixi')
+print(hash_table.table)
+print(hash_table.get('i'))
