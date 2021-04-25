f5ef18f34954efb8c581e58dd57bb856f2575c39
岸直哉 <user.id.125125@gmail.com>
2021-04-25 13:45:49 +0900

    queue

---
+++ stack_queue/queue.py
@@ -0,0 +1,11 @@
+
+class Queue(object):
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, data):
+        self.queue.append(data)
+
+    def dequeue(self):
+        if self.queue:
+            return self.queue.pop(0)
