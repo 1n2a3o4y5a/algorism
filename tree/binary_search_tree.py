a37892b5d1dfda422c80d3a875bed8a9b44449be
岸直哉 <user.id.125125@gmail.com>
2021-07-08 20:38:46 +0900

    remove

--- tree/binary_search_tree.py
+++ tree/binary_search_tree.py
@@ -28,3 +28,35 @@ def search(node, value):
         return search(node.legft, value)
     elif node.value < value:
         return search(node.right, value)
+
+def min_value(node):
+    current = node
+    while current.left is not None:
+        return current
+
+def remove(node, value):
+    if node is None:
+        return node
+
+    if value < node.value:
+        node.left = remove(node.left, value)
+    elif value > node.value:
+        node.right = remove(node.right, value)
+    else:
+        if node.left is None:
+            return node.right
+        elif node.right is None:
+            return node.left
+
+        temp = min_value(node.right)
+        node.value = temp.value
+        remove(node.right, temp.right)
+    
+    return node
+        
+
+test = None
+test = insert(test, 1)
+test = insert(test, 2)
+test = insert(test, 3)
+test = remove(test, 3)
