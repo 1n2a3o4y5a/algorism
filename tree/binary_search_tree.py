1d1308946c7a58eed25d65d8af5afb7538dfcdbf
岸直哉 <user.id.125125@gmail.com>
2021-07-13 08:56:46 +0900

    class化

--- tree/binary_search_tree.py
+++ tree/binary_search_tree.py
@@ -8,55 +8,67 @@ class Node(object):
 
 
 
+class BinarySearchTree():
+    def __init__(self) -> None:
+        self.root = None
 
-def insert(node, value):
-    if node is None:
-        return Node(value)
-    if value < node.value:
-        node.left = insert(node.left, value)
-    else:
-        node.right = insert(node.right, value)
-    return node
 
-def search(node, value):
-    if node is None:
-        return False
-    
-    if node.value == value:
-        return True
-    elif node.value > value:
-        return search(node.legft, value)
-    elif node.value < value:
-        return search(node.right, value)
+    def insert(self, value):
+        if self.root is None:
+            self.root = Node(value)
+            return
 
-def min_value(node):
-    current = node
-    while current.left is not None:
-        return current
+        def _insert(node, value):
+            if node is None:
+                return Node(value)
+            if value < node.value:
+                node.left = _insert(node.left, value)
+            else:
+                node.right = _insert(node.right, value)
+            return node
 
-def remove(node, value):
-    if node is None:
-        return node
+        _insert(self.root, value)
 
-    if value < node.value:
-        node.left = remove(node.left, value)
-    elif value > node.value:
-        node.right = remove(node.right, value)
-    else:
-        if node.left is None:
-            return node.right
-        elif node.right is None:
-            return node.left
+    def search(self, value):
+        def _search(node, value):
+            if node is None:
+                return False
+            
+            if node.value == value:
+                return True
+            elif node.value > value:
+                return _search(node.legft, value)
+            elif node.value < value:
+                return _search(node.right, value)
 
-        temp = min_value(node.right)
-        node.value = temp.value
-        remove(node.right, temp.right)
-    
-    return node
+        _search(self.root, value)
+
+    def min_value(self, node):
+        current = node
+        while current.left is not None:
+            return current
+
+    def remove(self, value):
+        def _remove(node, value):
+            if node is None:
+                return node
+
+            if value < node.value:
+                node.left = _remove(node.left, value)
+            elif value > node.value:
+                node.right = _remove(node.right, value)
+            else:
+                if node.left is None:
+                    return node.right
+                elif node.right is None:
+                    return node.left
+
+                temp = self.min_value(node.right)
+                node.value = temp.value
+                _remove(node.right, temp.right)
+            
+            return node
         
+        _remove(self.root, value)
 
-test = None
-test = insert(test, 1)
-test = insert(test, 2)
-test = insert(test, 3)
-test = remove(test, 3)
+        
